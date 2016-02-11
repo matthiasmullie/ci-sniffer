39d6161041e15adb551b68c929b6021d21b790df
Matthias Mullie <git@mullie.eu>
2016-02-11 21:53:49 +0100

    Compare CI data with git data

--- tests/EnvironmentTest.php
+++ tests/EnvironmentTest.php
@@ -4,6 +4,7 @@ namespace MatthiasMullie\CI\Tests;
 
 use MatthiasMullie\CI\Environment;
 use MatthiasMullie\CI\Factory;
+use MatthiasMullie\CI\Providers\None;
 use PHPUnit_Framework_TestCase;
 
 class EnvironmentTest extends PHPUnit_Framework_TestCase
@@ -21,15 +22,49 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
         $this->environment = $factory->getCurrent();
     }
 
-    /**
-     * There isn't much we can test in terms of actual responses: whatever
-     * environment we run it on, with every single commit, ... things will
-     * change. Repo, however, should remain the same. So let's check if we find
-     * a valid result there, then we at least know the environment detection
-     * works & repo can be retrieved correctly.
-     */
     public function testGetRepo()
     {
+        /**
+         * There isn't much we can test in terms of actual responses: whatever
+         * environment we run it on, with every single commit, ... things will
+         * change. Repo, however, should remain the same. So let's check if we
+         * find a valid result there, then we at least know the environment
+         * detection works & repo can be retrieved correctly.
+         */
         $this->assertContains('matthiasmullie/ci-environment', $this->environment->getRepo());
+
+        if ($this->isGitRepo()) {
+            $none = new None();
+            $this->assertEquals($none->getRepo(), $this->environment->getRepo());
+        }
+    }
+
+    public function testGetBranch()
+    {
+        if (!$this->isGitRepo()) {
+            $this->markTestSkipped("No git repo, can't compare.");
+        }
+
+        $none = new None();
+        $this->assertEquals($none->getBranch(), $this->environment->getBranch());
+    }
+
+    public function testGetCommit()
+    {
+        if (!$this->isGitRepo()) {
+            $this->markTestSkipped("No git repo, can't compare.");
+        }
+
+        $none = new None();
+        $this->assertEquals($none->getCommit(), $this->environment->getCommit());
+    }
+
+    /**
+     * @return bool
+     */
+    public function isGitRepo()
+    {
+        exec("git status", $output, $status);
+        return $status === 0;
     }
 }
