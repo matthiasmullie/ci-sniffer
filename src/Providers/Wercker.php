4dd546eeae851bd3c85eb6c0ed706a5e5ecf9031
Matthias Mullie <github@mullie.eu>
2016-02-16 13:47:39 +0100

    This should do?

--- src/Providers/Wercker.php
+++ src/Providers/Wercker.php
@@ -50,7 +50,7 @@ class Wercker implements Environment
      */
     public function getBranch()
     {
-        return getenv('WERCKER_GIT_BRANCH');
+        return $this->getPullRequest() === '' ? getenv('WERCKER_GIT_BRANCH') : '';
     }
 
     /**
@@ -68,8 +68,7 @@ class Wercker implements Environment
         // don't have permissions to do fetch all PR refs (like in None), but we
         // know Wercker has just fetched this particular PR...
         $head = shell_exec('cat .git/FETCH_HEAD');
-        var_dump($head);
-        preg_match("/^$commit\\s+'refs/pull/(.+)/head'/m", $head, $match);
+        preg_match("/^$commit\\s+'refs\\/pull\\/(.+)\\/head'/m", $head, $match);
 
         return isset($match[1]) ? $match[1] : '';
     }
